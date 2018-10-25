fef3faed8500fef03b4fbdebf145267fdc5b6ca2
Felix <fdenzing@students.uni-mainz.de>
2018-10-25 16:01:34 +0200

    test

--- src/lacto_FVA.py
+++ src/lacto_FVA.py
@@ -4,13 +4,14 @@ import cobra
 model = cobra.io.read_sbml_model("D:/Uni/Bioinformatik/Masterarbeit/iNF517.xml")
 model.solver = 'glpk'
 
+'''
 for reaction in model.reactions:
     # print(reaction.id, reaction.bounds)
     if reaction.id[:3] == "EX_":
         reaction.upper_bound = 1000.0
         reaction.lower_bound = -1000.0
-
-fva = flux_variability_analysis(model, model.reactions, fraction_of_optimum=0.9 ,loopless=True)
+'''
+fva = flux_variability_analysis(model, model.reactions, fraction_of_optimum=0.9, loopless=True)
 
 file = open("D:/Uni/Bioinformatik/Masterarbeit/lacto_fva.csv", 'w')
 
--- src/lacto_test.py
+++ src/lacto_test.py
@@ -1,35 +1,38 @@
 import cobra
 import Medium
+
 model = cobra.io.read_sbml_model("D:/Uni/Bioinformatik/Masterarbeit/iNF517.xml")
 
 # objective = model.reactions.get_by_id("BIOMASS_LLA_noATPnoH")
 
 # model.objective = objective
 
-model.solver = 'glpk'
+model.solver = 'cplex'
 
 min_medium = {"EX_glc__D_e": 1000.0,
-              "EX_arg__L_e": 1000.0,
-              "EX_cys__L_e": 1000.0,
-              "EX_ser__L_e": 1000.0,
-              "EX_thr__L_e": 1000.0,
+              "EX_asp__L_e": 1000.0,
               "EX_leu__L_e": 1000.0,
               "EX_met__L_e": 1000.0,
               "EX_val__L_e": 1000.0,
               "EX_ile__L_e": 1000.0,
               "EX_glu__L_e": 1000.0,
-              "EX_thymd_e": 1000.0,
+              "EX_ac_e": 1000.0,
+              "EX_fe2_e": 1000.0,
               "EX_fol_e": 1000.0,
               "EX_nac_e": 1000.0,
+              "EX_nmn_e": 1000.0,
               "EX_pi_e": 1000.0,
+              "EX_ppi_e": 1000.0,
               "EX_pnto__R_e": 1000.0,
+              "EX_ribflv_e": 1000.0,
+              "EX_thm_e": 1000.0,
               "EX_ura_e": 1000.0,
               "EX_orot_e": 1000.0,
               "EX_o2_e": 1000.0,
               "EX_h2o_e": 1000.0}
 
 min_medium2 = {"EX_2hxic__L_e": 10.0,
-               #"EX_acgam_e": 1.0,
+               # "EX_acgam_e": 1.0,
                "EX_glc__D_e": 100.0,
                "EX_cys__L_e": 10.0,
                "EX_glu__L_e": 20.0,
@@ -44,6 +47,32 @@ min_medium2 = {"EX_2hxic__L_e": 10.0,
                "EX_thymd_e": 10.0,
                "EX_o2_e": 1000.0,
                "EX_h2o_e": 1000.0}
+
+min_medium3 = {"EX_2mba_e": 1000.0,
+               "EX_ade_e": 1000.0,
+               "EX_arg__L_e": 1000.0,
+               "EX_asn__L_e": 1000.0,
+               "EX_asp__L_e": 1000.0,
+               "EX_cys__L_e": 1000.0,
+               "EX_drib_e": 1000.0,
+               "EX_glyc3p_e": 1000.0,
+               "EX_glc__D_e": 1000.0,
+               "EX_gua_e": 1000.0,
+               "EX_h2o_e": 1000.0,
+               "EX_h_e": 1000.0,
+               "EX_o2_e": 1000.0,
+               "EX_his__L_e": 1000.0,
+               "EX_lys__L_e": 1000.0,
+               "EX_nmn_e": 1000.0,
+               "EX_phe__L_e": 1000.0,
+               "EX_pnto__R_e": 1000.0,
+               "EX_ppi_e": 1000.0,
+               "EX_rib__D_e": 1000.0,
+               "EX_ser__L_e": 1000.0,
+               "EX_thymd_e": 1000.0,
+               "EX_trp__L_e": 1000.0,
+               "EX_tyr__L_e": 1000.0,
+               "EX_ura_e": 1000.0}
 
 medium_availability_factor = 1000000
 biomass = 1.0
@@ -74,7 +103,7 @@ for t in range(1):
                 reaction.lower_bound = -1 * medium.get_component(reaction.id)
             else:
                 reaction.lower_bound = 0
-            #print(reaction.id, reaction.bounds)
+                # print(reaction.id, reaction.bounds)
 
     try:
         solution = model.optimize(objective_sense='maximize', raise_error=True)
@@ -107,9 +136,11 @@ for t in range(1):
     else:
         break
 
+'''
 file = open("D:/Uni/Bioinformatik/Masterarbeit/growth_lacto.csv", 'w')
 
 for i, g in enumerate(growth_curve):
     file.write(str(i) + ";" + str(round(g, 2)) + ";" + str(growth_rate[i]) + "\n")
 file.close()
 
+'''
